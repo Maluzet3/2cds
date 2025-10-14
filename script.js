80adb9a04162f28e2dea06c39f3b687731d95f76
Maluzet3 <girelli.maria@escola.pr.gov.br>
2025-10-14 10:34:57 -0300

    p3

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-let currentIndex = 0;
-
-function moveSlide(direction) {
-  const images = document.querySelectorAll(".carousel-images img");
-  const totalImages = images.length;
-  currentIndex = (currentIndex + direction + totalImages) % totalImages;
-}document.querySelector(".carousel-images").style.transform = `translateX(-${currentIndex * 100}%)`;
 
